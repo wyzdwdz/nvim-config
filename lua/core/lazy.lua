1df06aea4fc8c548361f968f1b348395c822b1ac
wyzdwdz <wyzdwdz@gmail.com>
2025-09-06 11:26:47 +0200

    update

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -12,8 +12,22 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-	},
-})
+local os_name = vim.loop.os_uname().sysname
+
+local spec = { import = "plugins" }
+
+if os_name == "Windows" or os_name == "Windows_NT" then
+	if vim.fn.isdirectory("../plugins/windows") == 1 then
+		table.insert(spec, { import = "plugins/windows" })
+	end
+elseif os_name == "Darwin" then
+	if vim.fn.isdirectory("../plugins/macos") == 1 then
+		table.insert(spec, { import = "plugins/macos" })
+	end
+elseif os_name == "Linux" then
+	if vim.fn.isdirectory("../plugins/linux") == 1 then
+		table.insert(spec, { import = "plugins/linux" })
+	end
+end
+
+require("lazy").setup({ spec })
