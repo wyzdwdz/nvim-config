be3608ec91ff405b4a693aee0611bc3c191e1797
wyzdwdz <wyzdwdz@gmail.com>
2025-10-08 21:38:32 +0200

    update

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
+vim.g.mapleader = ","
+vim.g.maplocalleader = "."
 
 vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true })
