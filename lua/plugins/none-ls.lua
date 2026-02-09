d261838535f8ef6696c0aea56510aeed1282ea36
wyzdwdz <wyzdwdz@gmail.com>
2026-02-09 14:15:04 +0100

    update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -19,6 +19,7 @@ return {
 		install("black")
 		install("cmakelang")
 		install("csharpier")
+        install("prettier")
 
 		local null_ls = require("null-ls")
 		null_ls.setup({
@@ -30,6 +31,7 @@ return {
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.csharpier,
+				null_ls.builtins.formatting.prettier,
 			},
 		})
 	end,
