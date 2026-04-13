7b98a24c21e30b46c1a9ca83121c5a46d5346f6e
wyzdwdz <wyzdwdz@gmail.com>
2026-04-13 10:33:33 +0200

    update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,40 +3,26 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		local ts = require("nvim-treesitter")
 
-			ensure_installed = {
-				"lua",
-				"toml",
-				"rust",
-				"toml",
-				"nix",
-				"javascript",
-				"typescript",
-				"tsx",
-				"css",
-				"html",
-				"markdown",
-				"markdown_inline",
-				"c",
-				"cpp",
-				"cmake",
-				"go",
-				"c_sharp",
-			},
-
-			sync_install = false,
-
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-
-			indent = {
-				enable = true,
-			},
+		ts.install({
+			"lua",
+			"toml",
+			"rust",
+			"toml",
+			"nix",
+			"javascript",
+			"typescript",
+			"tsx",
+			"css",
+			"html",
+			"markdown",
+			"markdown_inline",
+			"c",
+			"cpp",
+			"cmake",
+			"go",
+			"c_sharp",
 		})
 	end,
 }
