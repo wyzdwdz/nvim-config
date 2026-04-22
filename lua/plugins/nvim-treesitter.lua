0a7c3622cf09a752536a040f981aadb5d420c759
wyzdwdz <wyzdwdz@gmail.com>
2026-04-22 19:34:55 +0200

    update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		local ts = require("nvim-treesitter")
-
-		ts.install({
+	event = "VeryLazy",
+	branch = "master",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
 			"lua",
 			"toml",
 			"rust",
@@ -23,21 +22,7 @@ return {
 			"cmake",
 			"go",
 			"c_sharp",
-		})
-
-		local ts_group = vim.api.nvim_create_augroup("GlobalTreesitter", { clear = true })
-
-		vim.api.nvim_create_autocmd("FileType", {
-			group = ts_group,
-			callback = function(args)
-				local ok = pcall(vim.treesitter.start, args.buf)
-
-				if ok then
-					vim.opt_local.foldmethod = "expr"
-					vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-					vim.opt_local.foldenable = false
-				end
-			end,
-		})
-	end,
+		},
+		highlight = { enable = true },
+	},
 }
