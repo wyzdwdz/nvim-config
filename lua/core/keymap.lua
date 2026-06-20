d0523634da54b0ad58c7a96bf0933087477234b8
wyzdwdz <wyzdwdz@gmail.com>
2026-06-20 22:39:45 +0200

    update

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -3,3 +3,11 @@ vim.g.maplocalleader = ""
 
 vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true })
+
+vim.keymap.set("n", "j", function()
+	if vim.fn.line(".") == vim.fn.line("$") then
+		return "<C-e>"
+	else
+		return "j"
+	end
+end, { expr = true, replace_keycodes = true, desc = "Scroll view on 'j' at EOF" })
